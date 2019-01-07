ddc47f251f0b7fcc1e9e1ead7fd8070ac45e5ca3
Vadim Patsev <vadim@smekalka.com>
2019-01-07 18:15:40 +0300

    Updates

---
+++ precache-manifest.53cfbfe1af0237032083431cca743c52.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/yet-another-react-test-app/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/yet-another-react-test-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/yet-another-react-test-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/yet-another-react-test-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "0ee7c4bf21ceb866e9a3",
+    "url": "/yet-another-react-test-app/static/js/runtime~main.0ee7c4bf.js"
+  },
+  {
+    "revision": "2d572705ffb04c591fb6",
+    "url": "/yet-another-react-test-app/static/js/main.2d572705.chunk.js"
+  },
+  {
+    "revision": "d01559c07a2d3f1804ce",
+    "url": "/yet-another-react-test-app/static/js/1.d01559c0.chunk.js"
+  },
+  {
+    "revision": "2d572705ffb04c591fb6",
+    "url": "/yet-another-react-test-app/static/css/main.39299e93.chunk.css"
+  },
+  {
+    "revision": "d01559c07a2d3f1804ce",
+    "url": "/yet-another-react-test-app/static/css/1.811c4918.chunk.css"
+  },
+  {
+    "revision": "c4a0178cee849d55d75e8bc856160e2a",
+    "url": "/yet-another-react-test-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.90faee893da7951025a10e70a85045c5.js"
+  "/yet-another-react-test-app/precache-manifest.53cfbfe1af0237032083431cca743c52.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/yet-another-react-test-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
