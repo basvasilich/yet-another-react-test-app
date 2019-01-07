c2937bc10f90c4d127f7d68fc48737f997ccca0e
Vadim Patsev <vadim@smekalka.com>
2019-01-07 18:18:43 +0300

    fix deploy

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const CONFIG_AVAILABLE_CURRENCIES = ['EUR', 'GBP', 'USD'];
-export const CONFIG_FIXER_API_URL = 'http://data.fixer.io/api/latest?access_key=f81c7ff6a7244887a367839c956f290b';
+export const CONFIG_FIXER_API_URL = 'https://data.fixer.io/api/latest?access_key=f81c7ff6a7244887a367839c956f290b';
